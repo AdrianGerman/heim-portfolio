ba7312d8bbd24ce5083bd6b935644aeed0287f13
AdrianGerman <adrian.german1019@gmail.com>
2024-09-30 08:59:10 -0600

    new font for titles and ids

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        "Onest-Variable": ["Onest Variable", "sans-serif"]
+      },
       colors: {
         primary: "#76428a"
       }
